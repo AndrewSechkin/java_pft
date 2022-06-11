880c77b9bb5f279be108018aa11c5a689d514b1d
AndrewSechkin <andrew.sechkin@yandex.ru>
2022-06-11 16:32:30 +0300

    Изменены настройки форматирования

--- sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
@@ -1,9 +1,8 @@
 package ru.stqa.pft.sandbox;
 
 public class MyFirstProgram {
-	
-	public static void main(String[] args) {
-	System.out.println("Hello, World!");
-}
+  public static void main(String[] args) {
+    System.out.println("Hello, World!");
+  }
 
 }
