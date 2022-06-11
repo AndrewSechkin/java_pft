c4932f8c3f9014128c80f861916aa991392f700b
AndrewSechkin <andrew.sechkin@yandex.ru>
2022-06-11 15:58:52 +0300

    Update MyFirstProgram.java

---
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
@@ -0,0 +1,17 @@
+<<<<<<< HEAD
+package ru.stqa.pft.sandbox;
+=======
+<<<<<<<< HEAD:sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
+package ru.stqa.pft.sandbox;
+========
+package ru.stqa.sandbox;
+>>>>>>>> 676c646e9f2ad12f014373333fb9c98104d0ba6a:sandbox/src/main/java/ru/stqa/sandbox/MyFirstProgram.java
+>>>>>>> e1fbd2021ab34e231e1126de1301ee2f9432058a
+
+public class MyFirstProgram {
+	
+	public static void main(String[] args) {
+	System.out.println("Hello, World!");
+}
+
+}
