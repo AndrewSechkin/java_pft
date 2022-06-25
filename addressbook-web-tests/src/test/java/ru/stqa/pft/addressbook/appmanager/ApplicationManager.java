c4d018cb5e1243aa2d905fbfe469fb4e22ebe28c
AndrewSechkin <andrew.sechkin@yandex.ru>
2022-06-25 15:15:17 +0300

    Задание 7
    Реализована возможность запуска тестов в Chrome и FireFox

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/ApplicationManager.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/ApplicationManager.java
@@ -2,24 +2,36 @@ package ru.stqa.pft.addressbook.appmanager;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.remote.BrowserType;
 
 import java.util.concurrent.TimeUnit;
 
 public class ApplicationManager {
-  WebDriver dr;
-
+  public WebDriver dr;
   private SessionHelper sessionHelper;
   private NavigationHelper navigationHelper;
-  private GroupHelper groupHelper;
   private ContactHelper contactHelper;
+  private GroupHelper groupHelper;
+  private final String browser;
+
+  public ApplicationManager(String browser) {
+    this.browser = browser;
+  }
 
   public void init() {
-    dr = new ChromeDriver();
+    if (browser.equals(BrowserType.CHROME)) {
+      System.setProperty("web-driver.chrome.driver", "C:\\Project/chromedriver");
+      dr = new ChromeDriver();
+    } else if (browser.equals(BrowserType.FIREFOX)) {
+      System.setProperty("web-driver.gecko.driver", "C:\\Project/geckodriver");
+      dr = new FirefoxDriver();
+    }
     dr.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
-    dr.get("http://localhost/addressbook/");
+    dr.get("http://localhost/addressbook");
     groupHelper = new GroupHelper(dr);
-    navigationHelper = new NavigationHelper(dr);
     contactHelper = new ContactHelper(dr);
+    navigationHelper = new NavigationHelper(dr);
     sessionHelper = new SessionHelper(dr);
     sessionHelper.login("admin", "secret");
   }
@@ -32,11 +44,15 @@ public class ApplicationManager {
     return groupHelper;
   }
 
+  public ContactHelper getContactHelper() {
+    return contactHelper;
+  }
+
   public NavigationHelper getNavigationHelper() {
     return navigationHelper;
   }
 
-  public ContactHelper getContactHelper() {
-    return contactHelper;
+  public SessionHelper getSessionHelper() {
+    return sessionHelper;
   }
 }
